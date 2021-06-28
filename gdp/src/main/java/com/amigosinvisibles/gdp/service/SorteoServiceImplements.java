5e25f7cbb23316506542d3f651d9d1d1032ee17d
julito1998 <zabalajulian2010@hotmail.com>
2021-06-28 01:23:25 -0300

    cambios en el tipo de sorteo

--- gdp/src/main/java/com/amigosinvisibles/gdp/service/SorteoServiceImplements.java
+++ gdp/src/main/java/com/amigosinvisibles/gdp/service/SorteoServiceImplements.java
@@ -2,17 +2,23 @@ package com.amigosinvisibles.gdp.service;
 
 import com.amigosinvisibles.gdp.model.Grupo;
 import com.amigosinvisibles.gdp.model.SorteoAsignacion;
+import com.amigosinvisibles.gdp.model.User;
 import com.amigosinvisibles.gdp.repository.SorteoAsignacionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import java.util.*;
 
 @Service
 public class SorteoServiceImplements  implements ISorteoAsignacionService{
 
     @Autowired
     private SorteoAsignacionRepo sorteoAsignacionRepo;
+
+    @Autowired
+    private IGrupoService grupoService;
+
+    @Autowired
+    private IUserService userService;
 
     @Override
     public List<SorteoAsignacion> listAsignados(Long idUser) throws Exception {
@@ -23,9 +29,49 @@ public class SorteoServiceImplements  implements ISorteoAsignacionService{
         }
     }
 
+
+    private void realizarSorteo(List<Grupo> grupos){
+            ArrayList<SorteoAsignacion> sorteoAsignacions= new ArrayList<>();
+            for (Grupo grupo:grupos){
+                try {
+                    Random random = new Random();
+
+                    ArrayList<User> users = new ArrayList<>(userService.listAllGrupo(grupo.getId()));
+
+                    x = Math.random()*users.length+0);
+                }atch (Exception e)c
+
+                }
+            }
+    }
+
+    private void guardarSorteoAutomatico(){
+        List<Grupo> grupoList= new ArrayList<>();
+
+        for (Grupo g: grupoService.listAll()) {
+            switch (String.valueOf(g.getTipoSorteo().getId())) {
+                // el tipo de sorteo es por fecha
+                case "1":
+                    if (g.getTipoSorteo().getFechaDelSorteo().after(new Date())) grupoList.add(g);
+                    break;
+                //el admin realizo el sorteo
+                case "2":
+                    break;
+                //el sorteo se realiza cuando se llega a una cantidad maxima de participantes
+                case "3":
+                    if(g.getCapacidadMaxima()==g.getTipoSorteo().getCapacidadMaxima()) grupoList.add(g);
+                    break;
+                default:
+                    break;
+            }
+        }
+        realizarSorteo(grupoList);
+    }
+
     @Override
     public SorteoAsignacion guardarAsignacionDelSorteo(SorteoAsignacion sorteoAsignacion) throws Exception {
         try{
+
             //crear algoritmo al azar entre los distintos usuarios
             //crear la ejecucion automatica del sorteo cada 24 horas, dependiendo del tipo
             //fecha limite, capacidad maxima o el admin presiona el botton sortear
